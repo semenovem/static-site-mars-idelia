b62fed7f3cbd5716cffac8c5e2785e2c230af61a
Semenov Evgeny <semenovem@gmail.com>
2019-03-11 21:24:38 +0300

    keep progress

---
+++ _includes/mod/videos/j.js
@@ -0,0 +1,36 @@
+app.execute({
+
+  init() {
+
+    app.domContentLoaded.then(this._init.bind(this));
+  },
+
+  _init() {
+    console.log('videos');
+
+    const els = document.querySelectorAll('[data-video]');
+
+  },
+
+
+
+  // загрузить плеер
+
+
+  // поставить обработчики на preview
+
+  // preview, которое находится в player - затемнить
+
+
+
+  _findElPlayers() {
+    document.querySelectorAll('[data-video]');
+
+  }
+
+
+  // data-video="{{ include.type }}"
+
+
+
+});
